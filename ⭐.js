cf76d9c6876fbf111a7457c7d31de65a55575e0e
Jazmine Abdilahi <66798977+Jazmine-Abdilahi@users.noreply.github.com>
2022-02-06 18:02:41 -0600

    Add files via upload

---
+++ ⭐.js
@@ -0,0 +1,29 @@
+/// video 59 - 62
+
+const mesureJazmine = function () {
+    const mesurement = f
+    type: 'temp',
+    unit: 'celcuis',
+    value: prompt('Degrees celcius:'),
+};
+
+const Jazmine = measurement.valu + 273;
+return jazmine;
+};
+
+console.log(measureJazmine());
+
+const jazmine = measurement.value + 273;
+return jazmine;
+};
+
+console.log(measureJazmine());
+
+///////////////////////////////////////////////////
+
+const data1 = [hi]
+const data2 = [hello]
+
+console.log(`say ${data1} or ${data2}`)
+
+/// min 7:00
